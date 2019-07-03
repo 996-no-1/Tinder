7aa3cecada3ab2a9599ea25742b3b6e106273f41
DesmondCobb <52391039+DesmondCobb@users.noreply.github.com>
2019-07-03 10:00:12 +0800

    [code] create group chat function -- 杜少恒

---
+++ source_code/src/ServerImp/GroupChatFunc.java
@@ -0,0 +1,85 @@
+package ServerImp;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.sun.org.apache.bcel.internal.generic.IREM;
+
+import Client.FileSend;
+import Client.Message;
+import db.dao.GroupDao;
+import db.entity.Group;
+
+public class GroupChatFunc {
+	
+	public static void main(String[] args) {
+		String a = "name[Group]";
+		System.err.println(a.split("\\[")[1]);
+	}
+	
+	private Map<String, ObjectOutputStream> oos = null;
+	
+	public GroupChatFunc(Map<String, ObjectOutputStream> oos){
+		this.oos = oos;
+	}
+
+	public void processGroupChat(Message msg) throws IOException {
+		
+		String fromName = msg.getSender(),groupName = msg.getReceiver().split("\\[")[0];
+		List<String> toList;
+		
+		if(fromName.equals("Admin")) {
+			
+		}else {
+			toList = getToList(groupName);
+			transferMsg(fromName,msg,toList);
+		}
+		
+	}
+
+	private void transferCreate(String fromName,String GroupName,String[] toList) {
+		
+	}
+	
+	private void transferMsg(String fromName,Message msg,List<String> toList) throws IOException {
+		
+		for (String ite : toList) {
+			if(ite.equals(fromName)) continue;
+			oos.get(ite).writeObject(msg);oos.get(ite).flush();
+		}
+		
+	}
+
+	private void transferLogout() {
+		
+	}
+	
+	private List<String> getToList(String groupName) {
+		List<String> toList = new ArrayList<String>();
+		GroupDao gdao = new GroupDao();
+		List<Group> groupList = gdao.getAllGroup();
+		for (Group group : groupList) {
+			if(groupName.equals(group.getGroupname())) {
+				toList = group.getMemberUsernameList();
+			}
+		}
+		
+		return toList;
+	}
+	
+	public List<List<String>> getTotalMemberGroup(){
+		List<List<String>> res = new ArrayList<List<String>>();
+		GroupDao gdao = new GroupDao();
+		List<Group> groupList = gdao.getAllGroup();
+		for (Group group : groupList) {
+			
+		}
+		
+		return res;
+	}
+
+}
