48306b13477661913e0b6f1c7c9108b403ceded8
DesmondCobb <52391039+DesmondCobb@users.noreply.github.com>
2019-07-04 09:45:54 +0800

    dev

--- src/src/db/dao/MessageDao.java
+++ src/src/db/dao/MessageDao.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import db.entity.GroupMessage;
 import db.entity.UserMessage;
 import db.util.DBFactory;
 import db.util.TimeFactory;
@@ -16,7 +17,7 @@ import db.util.TimeFactory;
  * user-to-user message)
  * 
  * @author DesmondCobb
- * @version 20190703.2224
+ * @version 20190704.0943
  *
  */
 public class MessageDao {
@@ -125,6 +126,104 @@ public class MessageDao {
 				long time = getRs.getLong(4);
 				UserMessage aUserMessage = new UserMessage(from, to, message, time);
 				messageList.add(aUserMessage);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return messageList;
+	}
+
+	/**
+	 * Save a user-to-user message to database
+	 * 
+	 * @param groupname
+	 * @param sender
+	 * @param message
+	 * @return
+	 */
+	public boolean saveGroupMessage(String groupname, String sender, String message) {
+		boolean status = false;
+		try {
+			Connection conn = DBFactory.getINSTANCE().getConnection();
+			long currentTime = TimeFactory.getTime();
+
+			String insSql = "insert into group_message values(?,?,?,?)";
+			PreparedStatement insPs = conn.prepareStatement(insSql);
+			insPs.setString(1, groupname);
+			insPs.setString(2, sender);
+			insPs.setString(3, message);
+			insPs.setLong(4, currentTime);
+			insPs.executeQuery();
+
+			status = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+
+	/**
+	 * Get all group messages of a group
+	 * 
+	 * @param groupname
+	 * @return
+	 */
+	public List<GroupMessage> getAllGroupMessage(String groupname) {
+		List<GroupMessage> messageList = new ArrayList<GroupMessage>();
+		try {
+			Connection conn = DBFactory.getINSTANCE().getConnection();
+
+			String getSql = "select * from group_message where groupname=?";
+			PreparedStatement getPs = conn.prepareStatement(getSql);
+			getPs.setString(1, groupname);
+			ResultSet getRs = getPs.executeQuery();
+
+			while (getRs.next()) {
+				String sender = getRs.getString(2);
+				String message = getRs.getString(3);
+				long time = getRs.getLong(4);
+				GroupMessage aGroupMessage = new GroupMessage(groupname, sender, message, time);
+				messageList.add(aGroupMessage);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return messageList;
+	}
+
+	/**
+	 * Get latest group message in 30 minutes
+	 * 
+	 * @param groupname
+	 * @return
+	 */
+	public List<GroupMessage> getLatestGroupMessageIn30Min(String groupname) {
+		List<GroupMessage> messageList = new ArrayList<GroupMessage>();
+		try {
+			Connection conn = DBFactory.getINSTANCE().getConnection();
+
+			String timeSql = "select max(time) from group_message where groupname=?";
+			PreparedStatement timePs = conn.prepareStatement(timeSql);
+			timePs.setString(1, groupname);
+			ResultSet timeRs = timePs.executeQuery();
+			if (!timeRs.next()) {
+				return messageList;
+			}
+			long latestMessageTime = timeRs.getLong(1);
+			long mark = TimeFactory.getTimeSomeWhatBefore(latestMessageTime);
+
+			String getSql = "select * from group_message where groupname=? and time>?";
+			PreparedStatement getPs = conn.prepareStatement(getSql);
+			getPs.setString(1, groupname);
+			getPs.setLong(2, mark);
+			ResultSet getRs = getPs.executeQuery();
+
+			while (getRs.next()) {
+				String sender = getRs.getString(2);
+				String message = getRs.getString(3);
+				long time = getRs.getLong(4);
+				GroupMessage aGroupMessage = new GroupMessage(groupname, sender, message, time);
+				messageList.add(aGroupMessage);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
